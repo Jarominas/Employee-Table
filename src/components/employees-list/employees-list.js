d6b6e900e724e9634636c6aee2e5bd65b240d5c8
Artur <artur.jarominas@gmail.com>
2023-02-24 17:35:53 +0200

    props

--- src/components/employees-list/employees-list.js
+++ src/components/employees-list/employees-list.js
@@ -1,12 +1,19 @@
 import EmployeesListItem from "../employees-list-item/employees-list-item";
 import './employees-list.css'
 
-const EmployeesList = () => {
+const EmployeesList = ({data}) => {
+
+    const elements = data.map (item => {
+
+        const {id, ...itemProps} = item;
+        return (
+            <EmployeesListItem key={id} {...itemProps}/>
+        )
+    });
+
     return (
         <ul className="app-list list-group">
-            <EmployeesListItem name="John S." salary={800}/>
-            <EmployeesListItem name="Kim M." salary={900}/>
-            <EmployeesListItem name="Anton C." salary={1000}/>
+           {elements}
         </ul>
     );
 }
