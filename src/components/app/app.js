1dee083f868493990d77529e39a63ecce2d67540
Artur <artur.jarominas@gmail.com>
2023-02-24 17:36:29 +0200

    props added

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -7,6 +7,13 @@ import EmployeesAddForm from '../employees-add-form/employees-add-form';
 import './app.css';
 
 function App() {
+
+    const data = [
+        {name: 'John S.', salary: 800, increase: false, id: 1},
+        {name: 'Kim M.', salary: 900, increase: true, id: 2},
+        {name: 'Stacy B.', salary: 1200, increase: false, id: 3},
+        {name: 'Anton C.', salary: 1700, increase: false, id: 4}
+    ];
     return (
         <div className="app">
             <AppInfo/>
@@ -16,7 +23,7 @@ function App() {
                 <AppFilter/>
             </div>
 
-            <EmployeesList/>
+            <EmployeesList data={data}/>
             <EmployeesAddForm/>
         </div>
     );
